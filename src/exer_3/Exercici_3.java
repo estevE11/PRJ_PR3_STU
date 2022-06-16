4660bcb6812664262250538d7fbe26ca62fe86bf
Roger Esteve <rogeresteve30@gmail.com>
2022-06-16 16:28:47 +0200

    exercici 3

--- src/exer_3/Exercici_3.java
+++ src/exer_3/Exercici_3.java
@@ -3,16 +3,32 @@ package exer_3;
 import java.util.Random;
 
 public class Exercici_3 {
-	
-	
+	private static Random alea = new Random();
+
 	public static void main (String [] args) {
 		/* Escriviu aquí el vostre codi de prova. Genereu parelles de
 		 * vectors i utilitzeu la vostra funció per dterminar si l'un 
 		 * és la versió girada de l'altre o no. Vegeu l'enunciat 
 		 * de la pràctica */
+		int n = 10;
+		boolean correct = true;
+		for(int i = 0; i < n; i++) {
+			boolean reversed = alea.nextBoolean();
+			VectorPair pair = new VectorPair(reversed);
+			boolean res = checkReversed(pair, 0);
+			if(res != reversed) correct = false;
+		}
+		System.out.println(correct ? "Correctes" : "Errors");
 	}
 	
 	/* ESCRIVIU AQUÍ LA VOSTRA FUNCIÓ DE VERIFICACIÓ */
+	public static boolean checkReversed(VectorPair pair, int n) {
+		if(n == pair.one.length) return true;
+		if(pair.one[n] == pair.two[pair.two.length-1-n]) {
+			return checkReversed(pair, n+1);
+		}
+		return false;
+	}
 	
 }
 
